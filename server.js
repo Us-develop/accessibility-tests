0f7f28ad66e8af191afdfee60f9492f63a0fc2ac
KerimGarbouj <kerimgarbouj@gmail.com>
2026-04-01 02:08:50 +0200

    Fix Failed to fetch

--- server.js
+++ server.js
@@ -322,7 +322,7 @@ const allowedOrigin = process.env.ALLOWED_ORIGIN || '*';
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-App-Password');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
